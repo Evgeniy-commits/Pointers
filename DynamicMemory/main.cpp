690cf0b5e7555271ed132697f812df40e4d48531
Admin <Admin@DESKTOP-8URQBFL>
2025-06-09 21:24:48 +0500

    DynamicMemory

--- DynamicMemory/main.cpp
+++ DynamicMemory/main.cpp
@@ -12,8 +12,30 @@ void main()
 	int* arr = new int[n]; 
 	FillRand(arr, n);
 	Print(arr, n);
+	n++;
+	int* arr1 = new int[n];
+	int value;
+	cout << "¬ведите добавл€емое число: "; cin >> value;
+	for (int i = 0; i < n - 1; i++)
+	{
+		arr1[i] = arr[i];
+	}
+	arr1[n - 1] = value;
+	Print(arr1, n);
+
+	int* arr2 = new int[n];
+	cout << "¬ведите добавл€емое число: "; cin >> value;
+	arr2[0] = value;
+	for (int i = 0; i < n - 1; i++)
+	{
+		arr2[i + 1] = arr[i];
+	}
+	Print(arr2, n);
 
 	delete[] arr;
+	delete[] arr1;
+	delete[] arr2;
+
 }
 
 void FillRand(int arr[], const int n)
